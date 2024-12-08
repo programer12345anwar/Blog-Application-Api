0cda8de57407594710c6ed53335fe887b97f9896
programer12345anwar <mdanwar40212@gmail.com>
2024-12-08 22:13:12 +0530

    Third commit

--- src/test/java/com/anwar/blog/BlogApplicationApiApplicationTests.java
+++ src/test/java/com/anwar/blog/BlogApplicationApiApplicationTests.java
@@ -1,13 +1,25 @@
 package com.anwar.blog;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.anwar.blog.repository.UserRepo;
 
 @SpringBootTest
 class BlogApplicationApiApplicationTests {
+	@Autowired
+	private UserRepo userRepo;
 
 	@Test
 	void contextLoads() {
+	}
+	@Test
+	public void repoTest() {
+		String className=this.userRepo.getClass().getName();
+		String packageName=this.userRepo.getClass().getPackageName();
+		System.out.println(className);
+		System.out.println(packageName);
 	}
 
 }
