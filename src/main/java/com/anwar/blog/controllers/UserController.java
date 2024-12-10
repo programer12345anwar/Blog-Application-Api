df2fe65ddc15bbe3faa205d72a0d1c009f945d6c
programer12345anwar <mdanwar40212@gmail.com>
2024-12-10 23:51:31 +0530

    Exceptions handeled

--- src/main/java/com/anwar/blog/controllers/UserController.java
+++ src/main/java/com/anwar/blog/controllers/UserController.java
@@ -1,14 +1,21 @@
 package com.anwar.blog.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity; // Correct import
-
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.anwar.blog.payloads.ApiResponse;
 import com.anwar.blog.payloads.UserDto;
 import com.anwar.blog.services.UserService;
 
@@ -26,8 +33,28 @@ public class UserController {
 	}
 
 	// PUT - update user
+	@PutMapping("/{userId}")
+	public ResponseEntity<UserDto> updateUser(@RequestBody UserDto userDto, @PathVariable("userId") Integer uid) {
+		UserDto updateUser = this.userService.updateUser(userDto, uid);
+		return ResponseEntity.ok(updateUser);
+	}
 
 	// DELETE - delete user
+	@DeleteMapping("/{userId}")
+	public ResponseEntity<ApiResponse> deleteUser(@PathVariable("userId") Integer uid) {
+		this.userService.deleteUser(uid);
+		return new ResponseEntity<ApiResponse>(new ApiResponse("User Deleted Successfully", true), HttpStatus.OK);
+	}
 
 	// GET - get user
+	@GetMapping("/")
+	public ResponseEntity<List<UserDto>> getAllUsers() {
+		return ResponseEntity.ok(this.userService.getAllUsers());
+	}
+
+	// GET-Single user
+	@GetMapping("/{userId}")
+	public ResponseEntity<UserDto> getSingleUser(@PathVariable Integer userId) {
+		return ResponseEntity.ok(this.userService.getUserById(userId));
+	}
 }
